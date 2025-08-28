be00baae822f51385f22077b870a93e17a51d992
Narigela-Apoorva <139985515+Narigela-Apoorva@users.noreply.github.com>
2025-08-28 18:03:02 +0530

    Add files via upload

---
+++ OpenAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function OpenAccount() {
+    return ( 
+       <div className='container text-center p-5'>
+        <h2>Open a Zerodha account</h2>
+        <p className='text-muted p-3'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+         <button style={{width:"18%", margin:"0 auto"}} className='p-2 btn btn-primary fs-5'>Sign up for free</button>
+       </div>
+     );
+}
+
+export default OpenAccount;
